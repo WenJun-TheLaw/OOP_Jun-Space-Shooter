5f1ca258b25a1ddb820dcf66b15442703d44480a
InstaKarma4 <lawwenjun@gmail.com>
2021-07-06 20:31:47 +0800

    Initial Push
    Added the root layout and welcome page

---
+++ src/main/scala/MainApp.scala
@@ -0,0 +1,37 @@
+package Jun
+import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
+import scalafx.scene.Scene
+import scalafx.Includes._
+import scalafxml.core.{NoDependencyResolver, FXMLView, FXMLLoader}
+import javafx.{scene => jfxs}
+import scalafx.stage.{Stage}
+import scalafx.scene.image.Image
+
+object MainApp extends JFXApp {
+  // transform path of RootLayout.fxml to URI for resource location.
+  val rootResource = getClass.getResourceAsStream("view/RootLayout.fxml")
+  // initialize the loader object.
+  val loader = new FXMLLoader(null, NoDependencyResolver)
+  // Load root layout from fxml file.
+  loader.load(rootResource)
+  // retrieve the root component BorderPane from the FXML 
+  val roots = loader.getRoot[jfxs.layout.BorderPane]
+  // initialize stage
+  stage = new PrimaryStage {
+    title = "Jun Space Shooter"
+    scene = new Scene {
+      root = roots
+    }
+  }
+
+  def showWelcome() = {
+    val resource = getClass.getResourceAsStream("view/Welcome.fxml")
+    val loader = new FXMLLoader(null, NoDependencyResolver)
+    loader.load(resource);
+    val roots = loader.getRoot[jfxs.layout.AnchorPane]
+    MainApp.roots.setCenter(roots)
+  } 
+  // call to display Welcome when app start
+  showWelcome()
+}
