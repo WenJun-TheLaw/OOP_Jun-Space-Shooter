57cc4d98cef184ff90d26d52dac2c28f0933a58c
InstaKarma4 <lawwenjun@gmail.com>
2021-07-08 19:03:55 +0800

    Test

--- src/main/scala/Jun/MainApp.scala
+++ src/main/scala/Jun/MainApp.scala
@@ -10,6 +10,13 @@ import scalafx.scene.image.Image
 import Jun.util.Database
 import Jun.model.Score
 import javafx.{scene => jfxs}
+import scalafx.scene.canvas.GraphicsContext
+import scalafx.scene.canvas.Canvas
+import Jun.model.Sprite
+import Jun.model.Player
+import scalafx.scene.input.KeyEvent
+import scalafx.scene.input.KeyCode
+import scalafx.animation.AnimationTimer
 
 object MainApp extends JFXApp {
   //initialize database
@@ -24,11 +31,10 @@ object MainApp extends JFXApp {
   loader.load(rootResource)
   // retrieve the root component BorderPane from the FXML 
   val roots = loader.getRoot[jfxs.layout.BorderPane]
-  var mainScene : Scene = new Scene(roots)
   // initialize stage
   stage = new PrimaryStage {
     title = "Jun Space Shooter"
-      scene = mainScene
+      scene = new Scene(roots)
     icons += new Image(getClass.getResourceAsStream("/Images/icon.png"))
   }
 
@@ -38,6 +44,7 @@ object MainApp extends JFXApp {
     loader.load(resource);
     val roots = loader.getRoot[jfxs.layout.AnchorPane]
     MainApp.roots.setCenter(roots)
+    initGame()
   } 
 
   def showGame() = {
@@ -51,4 +58,71 @@ object MainApp extends JFXApp {
 
   // call to display Welcome when app start
   showMainMenu()
+
+  /**
+   * Initialize the player and start game loop
+   */
+  def initGame() = {
+    //Setting up GraphicsContext
+    val scene = stage.getScene()
+    val canvas = 
+    var gc : GraphicsContext = canvas.graphicsContext2D
+    var lastTime = 0L
+    //val scene = MainApp.mainScene
+
+    //Player
+    val playerShip = new Image(getClass.getResourceAsStream("/Images/player_ship.png"))
+    val playerSprite = new Sprite(playerShip, 100, 200, 0, 0, playerShip.width.toDouble, playerShip.height.toDouble)
+    val player = new Player(100, 10, playerSprite, 1.0)
+    player.sprite.render(gc)
+
+    //Input
+    var leftPress = false
+    var rightPress = false
+    var upPress = false 
+    var downPress = false
+    scene.onKeyPressed = (key : KeyEvent) => {
+        if(key.code == KeyCode.W) upPress = true
+        if(key.code == KeyCode.A) leftPress = true
+        if(key.code == KeyCode.S) rightPress = true
+        if(key.code == KeyCode.D) downPress = true
+    }
+
+    scene.onKeyReleased = (key : KeyEvent) => {
+        if(key.getCode == KeyCode.W) upPress = false
+        if(key.getCode == KeyCode.A) leftPress = false
+        if(key.getCode == KeyCode.S) rightPress = false
+        if(key.getCode == KeyCode.D) downPress = false
+    }
+
+    val timer = AnimationTimer( t => {
+      def handle(currentNanoTime : Long){
+        if(lastTime > 0){
+          val delta = (t - lastTime)/ 1000000000.0; 
+
+          player.sprite.update(delta) 
+          player.sprite.render(gc)
+
+          if(upPress){
+            player.sprite.velocityY = 1 * player.speed
+            player.sprite.update(delta)
+          }
+          if(leftPress){
+            player.sprite.velocityX = -1 * player.speed
+            player.sprite.update(delta)
+          }
+          if(rightPress){
+            player.sprite.velocityX = 1 * player.speed
+            player.sprite.update(delta)
+          }
+          if(downPress){
+            player.sprite.velocityY = -1 * player.speed
+            player.sprite.update(delta)
+          }
+        }
+      }
+      lastTime = t;
+    })
+    timer.start()
+  }
 }
