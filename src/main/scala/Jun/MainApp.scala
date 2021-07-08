8c1dfdfe0044bb166b6270491ddab44ef01e8755
InstaKarma4 <lawwenjun@gmail.com>
2021-07-08 21:11:43 +0800

    PROGRESS!

--- src/main/scala/Jun/MainApp.scala
+++ src/main/scala/Jun/MainApp.scala
@@ -17,6 +17,7 @@ import Jun.model.Player
 import scalafx.scene.input.KeyEvent
 import scalafx.scene.input.KeyCode
 import scalafx.animation.AnimationTimer
+import scalafx.scene.layout.AnchorPane
 
 object MainApp extends JFXApp {
   //initialize database
@@ -44,7 +45,6 @@ object MainApp extends JFXApp {
     loader.load(resource);
     val roots = loader.getRoot[jfxs.layout.AnchorPane]
     MainApp.roots.setCenter(roots)
-    initGame()
   } 
 
   def showGame() = {
@@ -53,6 +53,7 @@ object MainApp extends JFXApp {
     loader.load(resource);
     val roots = loader.getRoot[jfxs.layout.AnchorPane]
     MainApp.roots.setCenter(roots)
+    initGame()
   }
 
 
@@ -63,12 +64,17 @@ object MainApp extends JFXApp {
    * Initialize the player and start game loop
    */
   def initGame() = {
+    val resource = getClass.getResourceAsStream("view/Game.fxml")
+    val loader = new FXMLLoader(null, NoDependencyResolver)
+    loader.load(resource);
+
     //Setting up GraphicsContext
     val scene = stage.getScene()
-    val canvas = 
+    val canvas = new Canvas(stage.width.value, stage.height.value)
+    scene.root = new AnchorPane(){
+      children = List(canvas)
+    } 
     var gc : GraphicsContext = canvas.graphicsContext2D
-    var lastTime = 0L
-    //val scene = MainApp.mainScene
 
     //Player
     val playerShip = new Image(getClass.getResourceAsStream("/Images/player_ship.png"))
@@ -82,24 +88,27 @@ object MainApp extends JFXApp {
     var upPress = false 
     var downPress = false
     scene.onKeyPressed = (key : KeyEvent) => {
-        if(key.code == KeyCode.W) upPress = true
-        if(key.code == KeyCode.A) leftPress = true
-        if(key.code == KeyCode.S) rightPress = true
-        if(key.code == KeyCode.D) downPress = true
+      println("Key Pressed")
+      if(key.code == KeyCode.W) upPress = true
+      if(key.code == KeyCode.A) leftPress = true
+      if(key.code == KeyCode.S) rightPress = true
+      if(key.code == KeyCode.D) downPress = true
     }
 
     scene.onKeyReleased = (key : KeyEvent) => {
-        if(key.getCode == KeyCode.W) upPress = false
-        if(key.getCode == KeyCode.A) leftPress = false
-        if(key.getCode == KeyCode.S) rightPress = false
-        if(key.getCode == KeyCode.D) downPress = false
+      println("Key Released")
+      if(key.getCode == KeyCode.W) upPress = false
+      if(key.getCode == KeyCode.A) leftPress = false
+      if(key.getCode == KeyCode.S) rightPress = false
+      if(key.getCode == KeyCode.D) downPress = false
     }
 
+
+    var startTime = System.nanoTime()
     val timer = AnimationTimer( t => {
       def handle(currentNanoTime : Long){
-        if(lastTime > 0){
-          val delta = (t - lastTime)/ 1000000000.0; 
-
+        println("Current Nano time: " + currentNanoTime)
+          val delta = (currentNanoTime - startTime)/ 1000000000.0; 
           player.sprite.update(delta) 
           player.sprite.render(gc)
 
@@ -119,10 +128,9 @@ object MainApp extends JFXApp {
             player.sprite.velocityY = -1 * player.speed
             player.sprite.update(delta)
           }
-        }
       }
-      lastTime = t;
     })
     timer.start()
+    
   }
 }
